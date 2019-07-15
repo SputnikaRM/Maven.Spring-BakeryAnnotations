32fcbbdf4e6ce50be987c350bc4ea6902ddd9bc0
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-07-15 16:48:09 -0400

    update

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -1,10 +1,16 @@
 package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Baker;
+import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.services.BakerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class BakerController {
     private BakerService service;
 
@@ -12,23 +18,28 @@ public class BakerController {
         this.service = service;
     }
 
+    @RequestMapping
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Baker> show(Long id) {
+    @RequestMapping
+    public ResponseEntity<Baker> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Baker> create(Baker baker) {
+    @RequestMapping
+    public ResponseEntity<Baker> create(@RequestParam Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Baker> update(Long id, Baker baker) {
+    @RequestMapping
+    public ResponseEntity<Baker> update(@PathVariable Long id, Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @RequestMapping
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
